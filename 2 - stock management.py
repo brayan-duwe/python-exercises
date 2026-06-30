d819cec17586802b2b1121bd669f4b9b364c615c
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-06-30 18:37:53 -0300

    try & except

--- 2 - stock management.py
+++ 2 - stock management.py
@@ -6,14 +6,20 @@ print("Number of smartphones in stock:", smartphones_stock)
 while True: 
     option = input("Press (1) to add smartphones to stock or (2) to remove from stock: ")
     if option == "1":
-        add_smartphones = input("Add smartphones \n Type the number: ")
-        smartphones_stock += int(add_smartphones)
-        break
+        try:
+            add_smartphones = input("Add smartphones \n Type the number: ")
+            smartphones_stock += int(add_smartphones)
+            break
+        except ValueError:
+            print("That wasn't an expected answer, try again.")
 
     elif option == "2":
-        remove_smartphones = input("Remove smartphones \n Type the number: ")
-        smartphones_stock -= int(remove_smartphones)
-        break
+        try:
+            remove_smartphones = input("Remove smartphones \n Type the number: ")
+            smartphones_stock -= int(remove_smartphones)
+            break
+        except ValueError:
+                print("That wasn't an expected answer, try again.")
 
     else:
         print("That wasn't an expected answer, try again.")
